3e58853757895092e378ef956e288950445c789d
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-06-25 20:47:14 +0800

    Started with JS

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const submit = document.getElementById('submit');
+
+submit.addEventListener("click", (e) => {
+    const day = parseInt(document.getElementById('day').value);
+    const month = parseInt(document.getElementById('month').value);
+    const year = parseInt(document.getElementById('year').value);
+});
